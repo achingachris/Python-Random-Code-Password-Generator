aab7864cd3ae226f39adb87ceba27adf22e70b0c
ChrisAchinga <achinga.chris@gmail.com>
2020-07-10 22:47:36 +0300

    lint

--- Code_Password Generator/password_generator.py
+++ Code_Password Generator/password_generator.py
@@ -1,22 +1,20 @@
-# import the random module 
+# import the random module
 import random
 
 # characters that would be used to create the passwords
 
-char = 'zcbnadgjlwryipXVBMKHFSQETUIO1230987645)(*&^%$#@)'
+char = "zcbnadgjlwryipXVBMKHFSQETUIO1230987645)(*&^%$#@)"
 
 # ask for password length
-length = input('Enter prefered password length: ')
-length = int(length) #convert input to integer
+length = input("Enter prefered password length: ")
+length = int(length)  # convert input to integer
 
-number = input('How many passwords? ')
+number = input("How many passwords? ")
 number = int(number)
 
 for p in range(number):
-# a variable where the password will be stored
-    password = ' '  #empty variable to add a character at a time
-
-
+    # a variable where the password will be stored
+    password = " "  # empty variable to add a character at a time
     for c in range(length):
         password += random.choice(char)
 
--- Code_Password Generator/unique_code_generator.py
+++ Code_Password Generator/unique_code_generator.py
@@ -1,10 +1,11 @@
 # import random
 import random
 
-name = input('Enter your name: ')
-char = 'ABCDEfghijkLMNOPQ123456789'
+name = input("Enter your name: ")
+char = "ABCDEfghijkLMNOPQ123456789"
 
-code = ' '
+code = " "
 for c in range(5):
     code += random.choice(char)
-print('Hello ' +name.upper() + ' Your ID number is ' + code)
+print("Hello " + name.upper() + " Your ID number is " + code)
+
